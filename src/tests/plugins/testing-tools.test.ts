621dd17a4f8126b5df9d780e2295805a781bea4a
Gumball12 <to@shj.rip>
2025-06-27 22:51:52 +0900

    fix: tests for testing tools

--- src/tests/plugins/testing-tools.test.ts
+++ src/tests/plugins/testing-tools.test.ts
@@ -1,3 +1,4 @@
+import type { YukiNoPlugin } from '../../plugins/core';
 import {
   createTestContext,
   loadPluginForTesting,
@@ -6,10 +7,25 @@ import {
 
 import { describe, expect, it, vi } from 'vitest';
 
+const mockExamplePlugin: YukiNoPlugin = {
+  name: 'yuki-no-test-plugin',
+  async onInit() {},
+  async onBeforeCompare() {},
+  async onAfterCompare() {},
+  async onBeforeCreateIssue() {},
+  async onAfterCreateIssue() {},
+  async onExit() {},
+  async onError() {},
+};
+
+vi.doMock('yuki-no-test-plugin', () => ({
+  default: mockExamplePlugin,
+}));
+
 describe('plugin testing tools', () => {
   it('loads plugin for testing', async () => {
-    const plugin = await loadPluginForTesting('yuki-no-plugin-example');
-    expect(plugin.name).toBe('yuki-no-example');
+    const plugin = await loadPluginForTesting('yuki-no-test-plugin');
+    expect(plugin.name).toBe('yuki-no-test-plugin');
   });
 
   it('creates test context', () => {
@@ -18,11 +34,13 @@ describe('plugin testing tools', () => {
   });
 
   it('runHook executes specified hook', async () => {
-    const plugin = await loadPluginForTesting('yuki-no-plugin-example');
+    const plugin = await loadPluginForTesting('yuki-no-test-plugin');
     const ctx = createTestContext();
-    const spy = vi.spyOn(plugin, 'onInit');
+
+    const spy = vi.spyOn(plugin, 'onInit' as any);
     await runHook(plugin, 'onInit', ctx);
-    expect(spy).toHaveBeenCalled();
+
+    expect(spy).toHaveBeenCalledWith(ctx);
     spy.mockRestore();
   });
 });
